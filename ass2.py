b70b5674501856d93959ff02bc64428f777d8a97
akshaychugh78 <37795763+akshaychugh78@users.noreply.github.com>
2018-06-30 22:38:50 +0530

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,39 @@
+# question 1
+
+print ('Welcome to the Virtual World')
+
+# question 2
+
+name = "akshay"
+lastname = "chugh"
+print (name + "kumar" + lastname)
+
+# question 3
+
+a = "akshay"
+b = 200
+c = "chugh"
+print (a,b,c)
+
+# question 4
+
+print ("Let's get started")
+
+# question 5
+
+s = "Acadview"
+course = "python"
+fees = 5000
+
+#%s for string
+#%d for decimal no.
+
+print ('%s %d %s'  %(s,fees,course))
+
+# question 6
+
+
+name = "Tony Stark"
+salary =100000
+# print('%s''%d')%(name,salary) *not working as given in assignment
+print ('%s%d'%(name,salary))
